bfbc8aab805de26801a81bc05884efaf493e8c3d
vincent <otienovincent1180@gmail.com>
2021-06-30 10:04:20 +0300

    keyboard

--- Softtech-Sacco/app/src/main/java/ke/co/softttech/lydia/softtech_sacco/Keyboard.java
+++ Softtech-Sacco/app/src/main/java/ke/co/softttech/lydia/softtech_sacco/Keyboard.java
@@ -11,10 +11,11 @@ import android.view.inputmethod.InputConnection;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Keyboard extends LinearLayout implements View.OnClickListener {
 
-    private Button button1, button2, button3, button4,
+    private TextView button1, button2, button3, button4,
             button5, button6, button7, button8,
             button9, button0;
     private ImageButton  buttonDelete, buttonEnter;
@@ -37,25 +38,25 @@ public class Keyboard extends LinearLayout implements View.OnClickListener {
 
     private void init(Context context, AttributeSet attrs) {
         LayoutInflater.from(context).inflate(R.layout.keyboard, this, true);
-        button1 = (Button) findViewById(R.id.button_1);
+        button1 =  findViewById(R.id.button_1);
         button1.setOnClickListener(this);
-        button2 = (Button) findViewById(R.id.button_2);
+        button2 =  findViewById(R.id.button_2);
         button2.setOnClickListener(this);
-        button3 = (Button) findViewById(R.id.button_3);
+        button3 = findViewById(R.id.button_3);
         button3.setOnClickListener(this);
-        button4 = (Button) findViewById(R.id.button_4);
+        button4 =  findViewById(R.id.button_4);
         button4.setOnClickListener(this);
-        button5 = (Button) findViewById(R.id.button_5);
+        button5 =  findViewById(R.id.button_5);
         button5.setOnClickListener(this);
-        button6 = (Button) findViewById(R.id.button_6);
+        button6 =  findViewById(R.id.button_6);
         button6.setOnClickListener(this);
-        button7 = (Button) findViewById(R.id.button_7);
+        button7 =  findViewById(R.id.button_7);
         button7.setOnClickListener(this);
-        button8 = (Button) findViewById(R.id.button_8);
+        button8 =  findViewById(R.id.button_8);
         button8.setOnClickListener(this);
-        button9 = (Button) findViewById(R.id.button_9);
+        button9 =  findViewById(R.id.button_9);
         button9.setOnClickListener(this);
-        button0 = (Button) findViewById(R.id.button_0);
+        button0 =  findViewById(R.id.button_0);
         button0.setOnClickListener(this);
         buttonDelete = (ImageButton) findViewById(R.id.button_delete);
         buttonDelete.setOnClickListener(this);
