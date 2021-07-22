1c6946d232534b7d1b8cbbb5d601dfe829dfdf43
Lydia Okech <okechlydia97@gmail.com>
2021-07-22 08:54:31 +0300

    click items

---
+++ Softtech-Sacco/app/src/main/java/ke/co/softttech/lydia/softtech_sacco/WithdrawActivity.java
@@ -0,0 +1,14 @@
+package ke.co.softttech.lydia.softtech_sacco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WithdrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw);
+    }
+}
